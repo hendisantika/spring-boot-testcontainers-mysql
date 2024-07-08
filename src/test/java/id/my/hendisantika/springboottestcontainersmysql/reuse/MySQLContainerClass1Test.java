5aac658396b34b4f3621ceb62283ab5b010abf84
hendisantika <hendisantika@yahoo.co.id>
2024-07-08 16:31:39 +0700

    Add MySQLContainerClass1Test🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springboottestcontainersmysql/reuse/MySQLContainerClass1Test.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springboottestcontainersmysql.reuse;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-testcontainers-mysql
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 7/8/24
+ * Time: 16:31
+ * To change this template use File | Settings | File Templates.
+ */
+public class MySQLContainerClass1Test extends MySQLContainerBaseTest {
+
+    @Test
+    void testMySQLContainerIsRunning() {
+        assertThat(mySQLContainer.isRunning()).isTrue();
+    }
+}
